7df3620776b83c213d7c60d1ac1eb9ed7d015d6d
kyleanicholson <kyle.nicholson67@gmail.com>
2023-08-03 16:41:47 -0700

    Create example data

---
+++ src/workoutData.js
@@ -0,0 +1,269 @@
+const workouts = [
+  {
+    title: "Bench Day",
+    muscle_groups: "Chest and Shoulders",
+    day_of_week: "Monday",
+    date: "2023-07-15",
+    exercises: [
+      {
+        exercise: "Bench Press",
+        sets: [
+          {
+            weight: 135,
+            reps: 10,
+            rpe: 8,
+            objectID: 0,
+          },
+          {
+            weight: 145,
+            reps: 8,
+            rpe: 7,
+            objectID: 1,
+          },
+          {
+            weight: 155,
+            reps: 6,
+            rpe: 9,
+            objectID: 2,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 0,
+      },
+      {
+        exercise: "Tricep Extension",
+        sets: [
+          {
+            weight: 50,
+            reps: 10,
+            rpe: 7,
+            objectID: 0,
+          },
+          {
+            weight: 55,
+            reps: 8,
+            rpe: 6,
+            objectID: 1,
+          },
+          {
+            weight: 60,
+            reps: 8,
+            rpe: 8,
+            objectID: 2,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 1,
+      },
+      {
+        exercise: "Shoulder Press",
+        sets: [
+          {
+            weight: 50,
+            reps: 10,
+            rpe: 6,
+            objectID: 0,
+          },
+          {
+            weight: 55,
+            reps: 8,
+            rpe: 7,
+            objectID: 1,
+          },
+          {
+            weight: 60,
+            reps: 6,
+            rpe: 8,
+            objectID: 2,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 2,
+      },
+    ],
+    objectID: 0,
+  },
+  {
+    title: "Leg Day",
+    muscle_groups: "Legs",
+    day_of_week: "Wednesday",
+    date: "2023-07-19",
+    exercises: [
+      {
+        exercise: "Squats",
+        sets: [
+          {
+            weight: 185,
+            reps: 8,
+            rpe: 7,
+            objectID: 0,
+          },
+          {
+            weight: 195,
+            reps: 6,
+            rpe: 8,
+            objectID: 1,
+          },
+          {
+            weight: 205,
+            reps: 6,
+            rpe: 9,
+            objectID: 2,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 0,
+      },
+      {
+        exercise: "Leg Press",
+        sets: [
+          {
+            weight: 225,
+            reps: 10,
+            rpe: 7,
+            objectID: 0,
+          },
+          {
+            weight: 235,
+            reps: 8,
+            rpe: 8,
+            objectID: 1,
+          },
+          {
+            weight: 245,
+            reps: 6,
+            rpe: 9,
+            objectID: 2,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 1,
+      },
+      {
+        exercise: "Leg Extension",
+        sets: [
+          {
+            weight: 50,
+            reps: 10,
+            rpe: 6,
+            objectID: 0,
+          },
+          {
+            weight: 55,
+            reps: 8,
+            rpe: 7,
+            objectID: 1,
+          },
+          {
+            weight: 60,
+            reps: 6,
+            rpe: 8,
+            objectID: 2,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 2,
+      },
+      {
+        exercise: "Leg Curl",
+        sets: [
+          {
+            weight: 50,
+            reps: 10,
+            rpe: 6,
+            objectID: 0,
+          },
+          {
+            weight: 55,
+            reps: 8,
+            rpe: 7,
+            objectID: 1,
+          },
+          {
+            weight: 60,
+            reps: 6,
+            rpe: 8,
+            objectID: 2,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 3,
+      },
+    ],
+    objectID: 1,
+  },
+  {
+    title: "Back Day",
+    muscle_groups: "Back, Biceps",
+    day_of_week: "Friday",
+    date: "2023-07-21",
+    exercises: [
+      {
+        exercise: "Deadlift",
+        sets: [
+          {
+            weight: 225,
+            reps: 5,
+            rpe: 8,
+            objectID: 0,
+          },
+          {
+            weight: 245,
+            reps: 4,
+            rpe: 9,
+            objectID: 1,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 0,
+      },
+      {
+        exercise: "Lat Pulldown",
+        sets: [
+          {
+            weight: 50,
+            reps: 10,
+            rpe: 7,
+            objectID: 0,
+          },
+          {
+            weight: 60,
+            reps: 8,
+            rpe: 8,
+            objectID: 1,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 1,
+      },
+      {
+        exercise: "Bicep Curl",
+        sets: [
+          {
+            weight: 50,
+            reps: 10,
+            rpe: 6,
+            objectID: 0,
+          },
+          {
+            weight: 55,
+            reps: 8,
+            rpe: 7,
+            objectID: 1,
+          },
+          {
+            weight: 60,
+            reps: 6,
+            rpe: 8,
+            objectID: 2,
+          },
+        ],
+        notes: "This is a note",
+        objectID: 2,
+      },
+    ],
+    objectID: 2,
+  },
+];
+
+export default workouts;

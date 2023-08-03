2cf9e5fecf257c076ef94ee2925829de73808533
kyleanicholson <kyle.nicholson67@gmail.com>
2023-08-02 20:03:45 -0700

    Add exercise prop, update workout and workout list

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import PropTypes from 'prop-types';
 
-const title = "The Workout Tracker";
+const title = "Gym Data Log";
+const subtitle = "Track your workouts and progress";
 const getTitle = (title) => title;
-// const exercise_list = [
-//   {
-//     exercise: "Bench Press",
-//     sets: 3,
-//     reps: 10,
-//     weight: 135,
-//     objectID: 0,
-//   },
-//   {
-//     exercise: "Squats",
-//     sets: 4,
-//     reps: 8,
-//     weight: 185,
-//     objectID: 1,
-//   },
-//   {
-//     exercise: "Deadlift",
-//     sets: 2,
-//     reps: 5,
-//     weight: 225,
-//     objectID: 2,
-//   },
-// ];
-
-
-
 const App = () => {
+
+  // Example data
   const workouts = [
   {
     title: "Bench Day",
@@ -38,10 +15,28 @@ const App = () => {
     day_of_week: "Monday",
     date: "2023-07-15",
     exercises: [
-      "Bench Press",
-      "Tricep Extension",
-      "Chin Ups",
-      "Side Lateral Raise",
+     {
+      exercise:"Bench Press",
+      sets: 3,
+      reps: 10,
+      weight: 135,
+      notes: "This is a note",
+      objectID: 0,
+      },{
+      exercise: "Tricep Extension",
+      sets: 3,
+      reps: 10,
+      weight: 50,
+      notes: "This is a note",
+      objectID: 1,
+      },{
+      exercise: "Shoulder Press",
+      sets: 3,
+      reps: 10,
+      weight: 50,
+      notes: "This is a note",
+      objectID: 2,
+      },
     ],
     objectID: 0,
   },
@@ -50,7 +45,37 @@ const App = () => {
     muscle_groups: "Legs",
     day_of_week: "Wednesday",
     date: "2023-07-19",
-    exercises: ["Squats", "Leg Press", "Lunges", "Calf Raises"],
+    exercises: [
+      {
+      exercise: "Squats",
+      sets: 4,
+      reps: 8,
+      weight: 185,
+      notes: "This is a note",
+      objectID: 0,
+      },{
+      exercise: "Leg Press",
+      sets: 3,
+      reps: 10,
+      weight: 225,
+      notes: "This is a note",
+      objectID: 1,
+      },{
+      exercise: "Leg Extension",
+      sets: 3,
+      reps: 10,
+      weight: 50,
+      notes: "This is a note",
+      objectID: 2,
+      },{
+      exercise: "Leg Curl",
+      sets: 3,
+      reps: 10,
+      weight: 50,
+      notes: "This is a note",
+      objectID: 3,
+      },
+    ],
     objectID: 1,
   },
   {
@@ -58,7 +83,30 @@ const App = () => {
     muscle_groups: "Back, Biceps",
     day_of_week: "Friday",
     date: "2023-07-21",
-    exercises: ["Deadlift", "Pull Ups", "Bent Over Row", "Bicep Curls"],
+    exercises:[ 
+      {
+      exercise:"Deadlift",
+      sets: 2,
+      reps: 5,
+      weight: 225,
+      notes: "This is a note",
+      objectID: 0,
+      },
+      {exercise: "Lat Pulldown",
+      sets: 3,
+      reps: 10,
+      weight: 50,
+      notes: "This is a note",
+      objectID: 1,
+      },
+      {exercise: "Bicep Curl",
+      sets: 3,
+      reps: 10,
+      weight: 50,
+      notes: "This is a note",
+    },
+    ],
+   
     objectID: 2,
   },
 
@@ -69,8 +117,10 @@ const handleSearch = (event) => {
 return (
     <div>
       <h1>Welcome to {getTitle(title)}!</h1>
+      <h2>{subtitle}</h2>
       <Search onSearch={handleSearch} />
-      <hr />
+      <h2>My Workouts</h2>
+      <button>Add Workout</button>
       <div id="workout-list">
         <WorkoutList workouts={workouts} />
       </div>
@@ -78,6 +128,54 @@ return (
   );
 };
 
+const Exercise = (props) => (
+  <li className="exercise-item">
+    <h4 className="exercise-title">{props.exercise.exercise}</h4>
+    <div className="exercise-details">
+      <span className="exercise-weight"> {props.exercise.weight} * </span>
+      <span className="exercise-sets"> {props.exercise.sets} sets of </span>
+      <span className="exercise-reps"> {props.exercise.reps} reps </span>
+      <p className="exercise-notes">Notes: {props.exercise.notes} </p>
+    </div>
+  </li>
+);
+
+Exercise.propTypes = {
+  exercise: PropTypes.string.isRequired,
+  sets: PropTypes.number.isRequired,
+  reps: PropTypes.number.isRequired,
+  weight: PropTypes.number.isRequired,
+  notes: PropTypes.string.isRequired,
+  objectID: PropTypes.number.isRequired,
+};
+const Workout = (props) => (
+  <li className="workout-item">
+    <hr />
+    <h3 className="workout-title">{props.workout.title}</h3>
+    <h4 className="workout-date">{props.workout.day_of_week}, {props.workout.date}</h4>
+    <hr />
+    <div className="workout-details">
+      <p className="workout-muscle-groups">Muscle Groups: {props.workout.muscle_groups}</p>
+    </div>
+    <ul className="workout-exercises">
+      {props.workout.exercises.map((exercise, index) => (
+        <li key={index} className="workout-exercise">
+          <Exercise exercise={exercise} />
+        </li>
+      ))}
+    </ul>
+  </li>
+);
+Workout.propTypes = {
+  workout: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    muscle_groups: PropTypes.string.isRequired,
+    day_of_week: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    exercises: PropTypes.arrayOf(PropTypes.shape(Exercise.propTypes)).isRequired,
+    objectID: PropTypes.number.isRequired,
+  }).isRequired,
+};
 const WorkoutList = (props) => (
   <ul>
     {props.workouts.map((workout) => (
@@ -88,43 +186,7 @@ const WorkoutList = (props) => (
 
 WorkoutList.propTypes = {
   workouts: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      muscle_groups: PropTypes.string.isRequired,
-      day_of_week: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      exercises: PropTypes.arrayOf(PropTypes.string).isRequired,
-      objectID: PropTypes.number.isRequired,
-    })
-  ).isRequired,
-};
-
-const Workout = (props) => (
-  <li className="workout-item">
-    <h4 className="workout-title">{props.workout.title}</h4>
-    <div className="workout-details">
-      <p className="workout-muscle-groups">Muscle Groups: {props.workout.muscle_groups}</p>
-      <p className="workout-date">Date: {props.workout.day_of_week}, {props.workout.date}</p>
-    </div>
-    <ul className="workout-exercises">
-      {props.workout.exercises.map((exercise, index) => (
-        <li key={index} className="workout-exercise">
-          {exercise}
-        </li>
-      ))}
-    </ul>
-  </li>
-);
-
-Workout.propTypes = {
-  workout: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    muscle_groups: PropTypes.string.isRequired,
-    day_of_week: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    exercises: PropTypes.arrayOf(PropTypes.string).isRequired,
-    objectID: PropTypes.number.isRequired,
-  }).isRequired,
+    PropTypes.shape(Workout.Proptypes)).isRequired,
 };
 
 const Search = (props) => {

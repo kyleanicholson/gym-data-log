5d70e8d987ece9a4dbe50225995ef08e36c99c8d
kyleanicholson <kyle.nicholson67@gmail.com>
2023-08-14 19:49:04 -0700

    Refactor, test exercise forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,56 @@
 import "./App.css";
 import Header from "./components/Header";
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 import Workouts from "./components/Workouts";
-import Stack from '@mui/material/Stack';
-import { useState } from 'react';
-import exampleWorkouts from './workoutData.js';
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+import exampleWorkouts from "./workoutData.js";
 import AddWorkoutModal from "./components/AddWorkoutModal";
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-
-
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import AddExerciseForm from "./components/AddExerciseForm";
+import Paper from "@mui/material/Paper";
+import AddSetForm from "./components/AddSetForm";
 
 const App = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isWorkoutModalOpen, setIsWorkoutModalOpen] = useState(false);
 
   const [workouts, setWorkouts] = useState(exampleWorkouts);
   const handleAddWorkout = (newWorkout) => {
-    setWorkouts(prevWorkouts => [...prevWorkouts, newWorkout]);
+    setWorkouts((prevWorkouts) => [...prevWorkouts, newWorkout]);
   };
 
   const handleDeleteWorkout = (workoutId) => {
-    setWorkouts(prevWorkouts => prevWorkouts.filter(workout => workout.objectID !== workoutId));
+    setWorkouts((prevWorkouts) =>
+      prevWorkouts.filter((workout) => workout.objectID !== workoutId)
+    );
   };
 
-  console.log(workouts)
+  console.log(workouts);
 
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-
-        <Container >
-          <Header onAddButtonClick={() => setIsModalOpen(true)} />
-          <AddWorkoutModal open={isModalOpen} onClose={() => setIsModalOpen(false)} existingWorkouts={workouts} onAdd={handleAddWorkout} />
+        <Container>
+          <Header onAddButtonClick={() => setIsWorkoutModalOpen(true)} />
+          <AddWorkoutModal
+            open={isWorkoutModalOpen}
+            onClose={() => setIsWorkoutModalOpen(false)}
+            existingWorkouts={workouts}
+            onAdd={handleAddWorkout}
+          />
           <Stack>
+            <Paper>
+              <AddExerciseForm></AddExerciseForm>
+              <AddSetForm></AddSetForm>
+            </Paper>
 
             <Workouts workouts={workouts} onDelete={handleDeleteWorkout} />
-
           </Stack>
-        </Container >
+        </Container>
       </LocalizationProvider>
     </>
-
   );
 };
-
 
 export default App;

9386546a9828a8d8bb59b5d4ba42d920073de764
kyleanicholson <kyle.nicholson67@gmail.com>
2023-08-21 17:58:23 -0700

    Improve form appearance and functionality

--- src/components/AddExerciseForm.jsx
+++ src/components/AddExerciseForm.jsx
@@ -1,39 +1,81 @@
-// import { TextField } from "@mui/material";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import AddSetForm from "./AddSetForm";
-import Button from '@mui/material/Button';
-// import { useState } from "react";
-// import MenuItem from "@mui/material/MenuItem";
-// import Select from "@mui/material/Select";
-// import InputLabel from "@mui/material/InputLabel";
-import Divider  from "@mui/material/Divider";
-
+import Button from "@mui/material/Button";
+import { useState, useEffect } from "react";
+import Divider from "@mui/material/Divider";
 import ExerciseLookup from "./ExerciseLookup";
 import TextField from "@mui/material/TextField";
+import PropTypes from "prop-types";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Typography } from "@mui/material";
 
-const AddExerciseForm = () => {
+const AddExerciseForm = ({ exercise, updateExercise }) => {
+  const [currentExercise, setCurrentExercise] = useState(exercise);
+  const [sets, setSets] = useState([{ weight: "", reps: "", rpe: "" }]);
+  const handleAddSet = () => {
+    setSets((prevSets) => [...prevSets, {}]);
+  };
+
+  useEffect(() => {
+    updateExercise(currentExercise);
+  }, [currentExercise, updateExercise]);
+
+  const handleSetUpdate = (index, updatedSet) => {
+    const updatedSets = [...sets];
+    updatedSets[index] = updatedSet;
+    setSets(updatedSets);
+  };
+  const handleRemoveSet = (index) => {
+    const newSets = [...sets];
+    newSets.splice(index, 1);
+    setSets(newSets);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("submitted");
   };
 
   return (
- 
-      <Container mb={2}>
-        <h2>Exercise</h2>
-        <form onSubmit={handleSubmit}>
-          <Box mb={2} mt={2}>  
-            <ExerciseLookup />
+    <form onSubmit={handleSubmit}>
+      <Box mb={2}>
+        <Typography variant="h5">Add Exercise</Typography>
+      </Box>
+      
+      <Box mb={2} mt={2}>
+        <ExerciseLookup
+          onExerciseSelect={(name) =>
+            setCurrentExercise({ ...currentExercise, name: name })
+          }
+        />
+      </Box>
+
+      <Box mb={2}>
+        <Typography variant="h6">Sets</Typography>
+        {sets.map((set, index) => (
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            key={index}
+          >
+            <AddSetForm index={index} onUpdate={handleSetUpdate} />
+            
+            <Button onClick={() => handleRemoveSet(index)} color="secondary">
+              <DeleteIcon />
+            </Button>
+         
+            {index === sets.length - 1 && (
+              <Button onClick={handleAddSet} variant="contained">
+                <AddIcon />
+              </Button>
+            )}
           </Box>
-          
-          <Box mb={2}>
-            <h3>Sets</h3>
-            <AddSetForm />
-          </Box>
-          
-          
-       <Box mb={2}>
+        ))}
+      </Box>
+
+      <Box mb={2}>
         <TextField
           id="outlined-multiline-static"
           label="Notes"
@@ -43,19 +85,21 @@ const AddExerciseForm = () => {
           variant="outlined"
           fullWidth
         />
-        </Box>
-         <Divider />
-          
-        <Box mb={2}>
-          <Button variant="contained" type="submit" fullWidth>Save Exercise</Button>
-          </Box>
+      </Box>
+      <Divider />
 
-  
-      
-        </form>
-      </Container>
-    
+      <Box mb={2}>
+        <Button variant="contained" type="submit" fullWidth>
+          Save Exercise
+        </Button>
+      </Box>
+    </form>
   );
+};
+
+AddExerciseForm.propTypes = {
+  exercise: PropTypes.object,
+  updateExercise: PropTypes.func,
 };
 
 export default AddExerciseForm;
--- src/components/AddSetForm.jsx
+++ src/components/AddSetForm.jsx
@@ -1,9 +1,25 @@
-import { Button, TextField } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import {TextField } from "@mui/material";
+
 import FormControl from "@mui/material/FormControl";
 import Stack from "@mui/material/Stack";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-const AddSetForm = () => {
+const AddSetForm = ({index, onUpdate}) => {
+
+  const [set, setSet] = useState({
+    weight: "",
+    reps: "",
+    rpe: "",
+  });
+
+  const handleChange = (field) => (event) => {
+    const newSet = { ...set, [field]: event.target.value };
+    setSet(newSet);
+    onUpdate(index, newSet);
+  };
+  
+
   return (
     <Stack sx = {{
       display: "flex",
@@ -21,14 +37,26 @@ const AddSetForm = () => {
         gap: "1rem",
       }}
     >
-      <TextField label="Weight" type="number"></TextField>
-      <TextField label="Reps" type="number"></TextField>
-      <TextField label="RPE" type="number"></TextField>
-      <Button type="submit" variant="contained" color="primary">
-      
-        <AddIcon></AddIcon>
-      </Button>
+       <TextField 
+          label="Weight" 
+          type="number"
+          value={set.weight}
+          onChange={handleChange('weight')}
+        />
+        <TextField 
+          label="Reps" 
+          type="number"
+          value={set.reps}
+          onChange={handleChange('reps')}
+        />
+        <TextField 
+          label="RPE" 
+          type="number"
+          value={set.rpe}
+          onChange={handleChange('rpe')}
+        />
        </FormControl>
+  
        
       
 
@@ -36,5 +64,11 @@ const AddSetForm = () => {
     </Stack>
   );
 };
+
+AddSetForm.propTypes = {
+  onUpdate: PropTypes.func,
+  index: PropTypes.number,
+};
+
 
 export default AddSetForm;

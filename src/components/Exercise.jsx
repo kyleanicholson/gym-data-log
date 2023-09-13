3d31350b685f460cebe6bee19ba9f1febf3749ae
kyleanicholson <kyle.nicholson67@gmail.com>
2023-09-12 21:25:31 -0700

    Improve styling of exercises

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -1,30 +1,43 @@
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import Set from './Set';
-import PropTypes from 'prop-types'
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Set from "./Set";
+import PropTypes from "prop-types";
+import { Box } from "@mui/material";
 
 const Exercise = ({ exercise }) => {
   return (
-    <div >
+    // Darken box background color
+    <Box
+      sx={{
+        backgroundColor: "#f5f5f5",
+        padding: "8px",
+        border: "1px solid #e0e0e0",
+        borderRadius: "8px",
+      }}
+    >
       <ListItem>
         <ListItemText
           primary={exercise.name}
-          primaryTypographyProps={{ variant: "h6" }}
+          primaryTypographyProps={{ variant: "h7" }}
         />
       </ListItem>
-
+      <Divider style={{ marginTop: "8px", marginBottom: "8px" }}></Divider>
       {exercise.sets.map((set, index) => (
-        <Set key={set.objectID} set={set} isLast={index === exercise.sets.length - 1} />
+        <Set
+          key={set.objectID}
+          set={set}
+          isLast={index === exercise.sets.length - 1}
+        />
       ))}
 
-      <Divider style={{ marginTop: '8px', marginBottom: '8px' }} /> {/* Space out exercises */}
-    </div>
-  )
-}
+      {/* Refactor this to use a table?  */}
+    </Box>
+  );
+};
 
 Exercise.propTypes = {
   exercise: PropTypes.object,
-}
+};
 
-export default Exercise
+export default Exercise;
--- src/components/Set.jsx
+++ src/components/Set.jsx
@@ -1,25 +1,23 @@
-import { Fragment } from 'react'
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import PropTypes from 'prop-types'
+import { Fragment } from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import PropTypes from "prop-types";
 
 const Set = ({ set, isLast }) => {
   return (
     <Fragment key={set.objectID}>
       <ListItem>
-        <ListItemText
-          secondary={`${set.weight} × ${set.reps} @ ${set.rpe}`}
-        />
+        <ListItemText secondary={`${set.weight} × ${set.reps} @ ${set.rpe}`} />
       </ListItem>
       {!isLast && <Divider />}
     </Fragment>
-  )
-}
+  );
+};
 
 Set.propTypes = {
   set: PropTypes.object,
   isLast: PropTypes.bool,
-}
+};
 
-export default Set
+export default Set;

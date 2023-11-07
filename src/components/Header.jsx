7db30ae9884c5d45f6c19174a6159d7c122b2bf8
CodeWithKyle <kyleanicholson@gmail.com>
2023-11-07 14:11:58 -0800

    Improve styling for mobile, add theme button and hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,66 @@
+import React from 'react';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import {
+    AppBar, Button, Toolbar, Typography, IconButton, Drawer, useTheme, useMediaQuery
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import PropTypes from 'prop-types';
+import DarkModeToggle from './DarkModeToggle';
 
-const Header = ({ onAddButtonClick }) => {
+const Header = ({onAddButtonClick, handleThemeChange, darkMode}) => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <FitnessCenterIcon sx={{ mr: 2 }} />
-        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-          Gym Data Log
-        </Typography>
-        <Button onClick={onAddButtonClick} color="inherit">Add Workout</Button>
-        <Button color="inherit">Log Out</Button>
-      </Toolbar>
-    </AppBar>
+    // State for opening and closing the Drawer
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
 
-  )
-}
+    const handleDrawerToggle = () => {
+        setDrawerOpen(!drawerOpen);
+    };
+
+    return (
+        <AppBar position="static" color="primary">
+            <Toolbar>
+                <FitnessCenterIcon sx={{mr: 2}}/>
+                <Typography variant="h5" component="div" sx={{flexGrow: 1, whiteSpace: 'nowrap'}}>
+                    Gym Data Log
+                </Typography>
+                {isMobile ? (<>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        onClick={handleDrawerToggle}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Drawer
+                        variant="temporary"
+                        open={drawerOpen}
+                        onClose={handleDrawerToggle}
+                        sx={{
+                            width: 'auto', // This sets the Drawer width to be automatic based on content
+                            '& .MuiDrawer-paper': {width: '80%', maxWidth: 300}, // Adjust as needed for your content
+                        }}
+                    >
+                        {/* Place your menu items here */}
+                        <Button onClick={onAddButtonClick} color="inherit">Add Workout</Button>
+                        <Button color="inherit">Log Out</Button>
+                    </Drawer>
+                </>) : (<>
+                    <Button onClick={onAddButtonClick} color="inherit">Add Workout</Button>
+                    <Button color="inherit">Log Out</Button>
+                </>)}
+                <DarkModeToggle handleThemeChange={handleThemeChange} darkMode={darkMode}/>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 Header.propTypes = {
-  onAddButtonClick: PropTypes.func,
-}
+    onAddButtonClick: PropTypes.func,
+    darkMode: PropTypes.bool.isRequired,
+    handleThemeChange: PropTypes.func.isRequired,
+};
 
-
-
-export default Header
+export default Header;

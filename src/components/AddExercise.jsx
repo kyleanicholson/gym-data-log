0262ce4b7ff3f8f4c6864ac54d2ec39f40f7e387
kyleanicholson <kyle.nicholson67@gmail.com>
2023-12-11 19:48:45 -0800

    Local commit only

--- src/components/AddExercise.jsx
+++ src/components/AddExercise.jsx
@@ -1,123 +1,129 @@
 import Box from "@mui/material/Box";
 import AddSet from "./AddSet";
 import Button from "@mui/material/Button";
-import { useState } from "react";
+import {useState} from "react";
 import Divider from "@mui/material/Divider";
 import TextField from "@mui/material/TextField";
 import PropTypes from "prop-types";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Typography } from "@mui/material";
+import {Typography} from "@mui/material";
 import FormControl from "@mui/material/FormControl";
+import {Stack} from "@mui/material";
 
 const AddExercise = (props) => {
-  const [sets, setSets] = useState([{ weight: "", reps: "", rpe: "" }]);
-  const [exercise, setExercise] = useState({
-    name: "",
-    notes: "",
-    sets: [],
-  });
+    const [sets, setSets] = useState([{weight: "", reps: "", rpe: ""}]);
+    const [exercise, setExercise] = useState({
+        name: "",
+        notes: "",
+        sets: [],
+    });
 
-  const handleChange = (field) => (event) => {
-    setExercise({ ...exercise, [field]: event.target.value });
-  };
+    const handleChange = (field) => (event) => {
+        setExercise({...exercise, [field]: event.target.value});
+    };
 
-  const handleAddSet = () => {
-    setSets((prevSets) => [...prevSets, {}]);
-  };
+    const handleAddSet = () => {
+        setSets((prevSets) => [...prevSets, {}]);
+    };
 
-  const handleSetUpdate = (index, updatedSet) => {
-    const updatedSets = [...sets];
-    updatedSets[index] = updatedSet;
-    setSets(updatedSets);
-    setExercise({ ...exercise, sets: updatedSets });
-  };
+    const handleSetUpdate = (index, updatedSet) => {
+        const updatedSets = [...sets];
+        updatedSets[index] = updatedSet;
+        setSets(updatedSets);
+        setExercise({...exercise, sets: updatedSets});
+    };
 
-  const handleSaveExercise = () => {
-    props.onAdd(exercise);
-  };
+    const handleSaveExercise = () => {
+        props.onAdd(exercise);
+    };
 
-  const handleRemoveSet = (index) => {
-    const newSets = [...sets];
-    newSets.splice(index, 1);
-    setSets(newSets);
-  };
+    const handleRemoveSet = (index) => {
+        const newSets = [...sets];
+        newSets.splice(index, 1);
+        setSets(newSets);
+    };
 
-  return (
-    <FormControl fullWidth>
-      <Box mb={2} mt={2}>
-        <TextField
-          required
-          value={exercise.name}
-          placeholder="Exercise Name"
-          type="text"
-          onChange={handleChange("name")}
-        />
-      </Box>
+    return (
+        <FormControl fullWidth>
+            <Box mb={2} mt={2}>
+                <TextField
+                    required
+                    value={exercise.name}
+                    placeholder="Exercise Name"
+                    type="text"
+                    onChange={handleChange("name")}
+                />
+            </Box>
 
-      <Box mb={2}>
-        <Typography variant="h6">Sets</Typography>
-        {sets.map((set, index) => (
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            key={index}
-          >
-            <AddSet index={index} onUpdate={handleSetUpdate} />
+            <Box mb={2}>
+                <Typography variant="h6" gutterBottom
+                >Sets</Typography>
+                <Stack spacing={1}>
+                    {sets.map((set, index) => (
+                        <Box
+                            display="flex"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            key={index}
+                        >
 
-            <Button onClick={() => handleRemoveSet(index)} color="secondary">
-              <DeleteIcon />
-            </Button>
+                            <AddSet index={index} onUpdate={handleSetUpdate}/>
 
-            {index === sets.length - 1 && (
-              <Button onClick={handleAddSet} variant="contained">
-                <AddIcon />
-              </Button>
-            )}
-          </Box>
-        ))}
-      </Box>
+                            <Button onClick={() => handleRemoveSet(index)} color="secondary">
+                                <DeleteIcon/>
+                            </Button>
 
-      <Box mb={2}>
-        <TextField
-          id="outlined-multiline-static"
-          label="Notes"
-          placeholder="Notes"
-          multiline
-          rows={4}
-          variant="outlined"
-          fullWidth
-          value={exercise.notes}
-          onChange={handleChange("notes")}
-        />
-      </Box>
-      <Divider />
+                            {index === sets.length - 1 && (
+                                <Button onClick={handleAddSet} variant="contained">
+                                    <AddIcon/>
+                                </Button>
+                            )}
 
-      <Box
-        display="flex"
-        justifyContent="flex-end"
-        alignItems="center"
-        mt={2}
-        mb={2}
-      >
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSaveExercise}
-        >
-          Save Exercise
-        </Button>
-      </Box>
-    </FormControl>
-  );
+                        </Box>
+                    ))}
+                </Stack>
+            </Box>
+
+            <Box mb={2}>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Notes"
+                    placeholder="Notes"
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    fullWidth
+                    value={exercise.notes}
+                    onChange={handleChange("notes")}
+                />
+            </Box>
+            <Divider/>
+
+            <Box
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="center"
+                mt={2}
+                mb={2}
+            >
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSaveExercise}
+                >
+                    Save Exercise
+                </Button>
+            </Box>
+        </FormControl>
+    );
 };
 
 AddExercise.propTypes = {
-  exercise: PropTypes.object,
-  updateExercise: PropTypes.func,
-  onUpdate: PropTypes.func,
-  onAdd: PropTypes.func,
+    exercise: PropTypes.object,
+    updateExercise: PropTypes.func,
+    onUpdate: PropTypes.func,
+    onAdd: PropTypes.func,
 };
 
 export default AddExercise;
--- src/components/AddSet.jsx
+++ src/components/AddSet.jsx
@@ -1,69 +1,60 @@
-import { TextField } from "@mui/material";
+import {TextField} from "@mui/material";
 
 import FormControl from "@mui/material/FormControl";
-import Stack from "@mui/material/Stack";
-import { useState } from "react";
+import {useState} from "react";
 import PropTypes from "prop-types";
 
-const AddSet = ({ index, onUpdate }) => {
-  const [set, setSet] = useState({
-    weight: "",
-    reps: "",
-    rpe: "",
-  });
+const AddSet = ({index, onUpdate}) => {
+    const [set, setSet] = useState({
+        weight: "",
+        reps: "",
+        rpe: "",
+    });
 
-  const handleChange = (field) => (event) => {
-    const newSet = { ...set, [field]: event.target.value };
-    setSet(newSet);
-    onUpdate(index, newSet);
-  };
+    const handleChange = (field) => (event) => {
+        const newSet = {...set, [field]: event.target.value};
+        setSet(newSet);
+        onUpdate(index, newSet);
+    };
 
-  return (
-    <Stack
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "1rem",
-        width: "100%",
-      }}
-    >
-      <FormControl
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          width: "100%",
-          gap: "1rem",
-        }}
-      >
-        <TextField
-          label="Weight"
-          type="number"
-          value={set.weight}
-          onChange={handleChange("weight")}
-        />
-        <TextField
-          label="Reps"
-          type="number"
-          value={set.reps}
-          onChange={handleChange("reps")}
-        />
-        <TextField
-          label="RPE"
-          type="number"
-          value={set.rpe}
-          onChange={handleChange("rpe")}
-        />
-      </FormControl>
-    </Stack>
-  );
+    return (
+            <>
+
+                <FormControl
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        width: "100%",
+
+                    }}
+                >
+                    <TextField
+                        label="Weight"
+                        type="number"
+                        value={set.weight}
+                        onChange={handleChange("weight")}
+                    />
+                    <TextField
+                        label="Reps"
+                        type="number"
+                        value={set.reps}
+                        onChange={handleChange("reps")}
+                    />
+                    <TextField
+                        label="RPE"
+                        type="number"
+                        value={set.rpe}
+                        onChange={handleChange("rpe")}
+                    />
+                </FormControl>
+            </>
+    );
 };
 
 AddSet.propTypes = {
-  onUpdate: PropTypes.func,
-  index: PropTypes.number,
+    onUpdate: PropTypes.func,
+    index: PropTypes.number,
 };
 
 export default AddSet;
